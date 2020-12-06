d9e11ecce5841a539972faf6f9b1857d896e4453
Your Name <oriya2699@gmail.com>
2020-12-06 12:19:11 +0200

    delete unused files

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../Views/view2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/MainView.fxml"));
         primaryStage.setTitle("Data Analyzer");
         primaryStage.setScene(new Scene(root, 600, 550));
         primaryStage.show();
